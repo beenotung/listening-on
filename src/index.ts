e83088391bb896d596b65e4f78e0055aa025c175
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-06-21 02:31:18 +0800

    do not show IPv6 address by default

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as os from 'os'
 
-export function print(port: number, protocol: string = 'http') {
+export function print(
+  port: number,
+  protocol: string = 'http',
+  family: 'IPv4' | 'IPv6' | 'all' = 'IPv4',
+) {
+  const showIPv4 = family === 'IPv4' || family === 'all'
+  const showIPv6 = family === 'IPv6' || family === 'all'
   let ifaces = os.networkInterfaces()
   Object.entries(ifaces).forEach(([name, ifaces]) => {
     ifaces?.forEach(iface => {
@@ -8,9 +14,9 @@ export function print(port: number, protocol: string = 'http') {
       if (address.startsWith('::')) {
         return
       }
-      if (family === 'IPv4') {
+      if (family === 'IPv4' && showIPv4) {
         console.log(`listening on ${protocol}://${address}:${port} (${name})`)
-      } else if (family === 'IPv6') {
+      } else if (family === 'IPv6' && showIPv6) {
         console.log(`listening on ${protocol}://[${address}]:${port} (${name})`)
       }
     })
