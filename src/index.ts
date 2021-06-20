90738ebaf1029071146022869b8bf3e6d75de6f7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-06-21 02:40:07 +0800

    lint fix

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ export function print(port_or_options: number | PrintOptions) {
   const family: Required<PrintOptions>['family'] = options.family || 'IPv4'
   const showIPv4 = family === 'IPv4' || family === 'all'
   const showIPv6 = family === 'IPv6' || family === 'all'
-  let ifaces = os.networkInterfaces()
+  const ifaces = os.networkInterfaces()
   Object.entries(ifaces).forEach(([name, ifaces]) => {
     ifaces?.forEach(iface => {
       let { address, family } = iface
