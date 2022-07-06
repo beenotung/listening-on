f8ed9ce66dd9d9f32db1d0f430a9480defacc9f2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-07-06 15:50:15 +0800

    support node18

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ export type Protocol = 'http' | 'https' | 'ws' | 'wss' | 'tcp' | 'udp'
 
 export type Family = 'IPv4' | 'IPv6'
 
+type Node16Family = 'IPv4' | 'IPv6'
+type Node18Family = 4 | 6
+type NodeFamily = Node16Family | Node18Family
+
 export function print(port_or_options: number | PrintOptions) {
   if (!port_or_options) {
     throw new TypeError('missing port or options')
@@ -29,11 +33,11 @@ export function print(port_or_options: number | PrintOptions) {
   const showIPv4 = family === 'IPv4' || family === 'all'
   const showIPv6 = family === 'IPv6' || family === 'all'
   Object.entries(networkInterfaces()).forEach(([name, addresses]) => {
-    addresses?.forEach(address => {
+    addresses?.forEach((address: { address: string; family: NodeFamily }) => {
       const { address: host, family } = address
-      if (family === 'IPv4' && showIPv4) {
+      if (showIPv4 && (family === 'IPv4' || family === 4)) {
         console.log(`listening on ${protocol}://${host}:${port} (${name})`)
-      } else if (family === 'IPv6' && showIPv6) {
+      } else if (showIPv6 && (family === 'IPv6' || family === 6)) {
         console.log(`listening on ${protocol}://[${host}]:${port} (${name})`)
       }
     })
